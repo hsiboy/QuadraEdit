ba456715a052d8deb13403f490514b22c0b6c930
ralley900 <ralley900@d8bc70b3-6028-4511-b56e-b43ad562b246>
2010-03-19 03:42:27 +0000

    Add source code directory

---
+++ src/error.h
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------------------
+#ifndef errorH
+#define errorH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+//---------------------------------------------------------------------------
+class TFormError : public TForm
+{
+__published:	// IDE-managed Components
+    TPanel *Panel1;
+    TLabel *LabelError;
+    TButton *ButtonOk;
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall FormHide(TObject *Sender);
+    void __fastcall ButtonOkClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+    __fastcall TFormError(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFormError *FormError;
+//---------------------------------------------------------------------------
+#endif
