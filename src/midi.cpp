418b268336b79e37dff6876e7d38d61721d127c3
ralley900 <ralley900@d8bc70b3-6028-4511-b56e-b43ad562b246>
2010-03-19 03:45:51 +0000

    Add source code directory

---
+++ src/midi.cpp
@@ -0,0 +1,220 @@
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#include <mmsystem.h>                       // we need this
+#pragma hdrstop
+
+#include "types.h"
+#include "midi.h"
+
+
+#define MIDI_IN_BUF_SIZE  (2048)
+
+
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+const UInt8 Sysex_Hdr[]={0xF0,              // SysEx
+                         0x00,0x00,0x0E,    // Alesis Manufacturer Id
+                         0x07};             // Quadraverb GT
+
+const UInt8 Sysex_Edit[]={0x01};                // Command to edit a Quadraverb function
+const UInt8 Sysex_Data_Dump[]={0x02};           // Command to send a program data dump
+const UInt8 Sysex_Dump_Req[]={0x03};            // Command to request a dump
+
+const UInt8 Sysex_End[]={0xF7};             // SysEx End
+
+// Midi handles
+static HMIDIOUT Midi_Out_Handle;
+static HMIDIIN Midi_In_Handle;
+
+// Headers and associated buffers for Midi Input SYSEX messages
+static MIDIHDR Midi_In_1;
+static UInt8 Midi_In_1_Buffer[MIDI_IN_BUF_SIZE];
+
+// Rx message counters
+static int rx_sysex=0;
+static int rx_msg=0;
+
+void Midi_Get_Dev_Lists(TComboBox *in_list, TComboBox *out_list, TLabel * error_text)
+{
+  int devs,i;
+  MMRESULT status;
+  MIDIOUTCAPS caps;
+  MIDIINCAPS incaps;
+
+  error_text->Caption="";
+  
+  /**************************************************************************
+  * Build the list of possible MIDI output devices
+  **************************************************************************/
+  devs = midiOutGetNumDevs();
+  if (devs < 1) {
+    error_text->Caption="No MIDI Output devices available";
+    // TBD: Handle error
+  }
+  else {
+    for (i=0; i<devs; i++)
+    {
+      status = midiOutGetDevCaps(i, &caps, sizeof(caps));
+      if (status == MMSYSERR_NOERROR)
+      {
+        out_list->Items->Add(caps.szPname);
+      }
+      else
+      {
+        out_list->Items->Add("MIDI Out "+AnsiString(i+1)+" - ERROR");
+      }
+    }
+  }
+
+  out_list->ItemIndex=0;
+
+  /**************************************************************************
+  * Build the list of possible MIDI input devices
+  **************************************************************************/
+  devs = midiInGetNumDevs();
+  if (devs < 1) {
+        error_text->Caption="No MIDI Input devices available";
+  }
+  else {
+    for (i=0; i<devs; i++)
+    {
+      status = midiInGetDevCaps(i, &incaps, sizeof(incaps));
+      if (status == MMSYSERR_NOERROR)
+      {
+        in_list->Items->Add(incaps.szPname);
+      }
+      else
+      {
+        in_list->Items->Add("MIDI In "+AnsiString(i+1)+" - ERROR");
+      }
+    }
+  }
+
+  in_list->ItemIndex=0;
+}
+
+UInt8 Midi_Out_Open(int device)
+{
+  MMRESULT status;
+  status=midiOutOpen(&Midi_Out_Handle, device, 0, 0, CALLBACK_NULL);
+
+  if (status == MMSYSERR_NOERROR)
+  {
+    return 0;
+  }
+  else
+  {
+    // TBD: report open error?
+    return 1;
+  }
+
+}
+
+void Midi_Out_Dump_Req(UInt8 program)
+{
+  MIDIHDR out;
+  UInt8 buffer[100];
+  UInt8 buf_len=0;
+
+  // Build message in buffer
+  memcpy(buffer, Sysex_Hdr, sizeof(Sysex_Hdr));
+  buf_len+=sizeof(Sysex_Hdr);
+
+  memcpy(buffer+buf_len, Sysex_Dump_Req, sizeof(Sysex_Dump_Req));
+  buf_len+=sizeof(Sysex_Dump_Req);
+
+  buffer[buf_len] = program;
+  buf_len+=1;
+
+  memcpy(buffer+buf_len, Sysex_End, sizeof(Sysex_End));
+  buf_len+=sizeof(Sysex_End);
+
+  // Prepare header
+  out.lpData = buffer;
+  out.dwBufferLength = buf_len;
+  out.dwBytesRecorded = buf_len;
+
+  // Prepare and send the MIDI message
+  midiOutPrepareHeader(Midi_Out_Handle, &out, sizeof(out));
+  midiOutLongMsg(Midi_Out_Handle, &out, sizeof(out));
+  midiOutUnprepareHeader(Midi_Out_Handle, &out, sizeof(out));
+
+}
+
+void Midi_Out_ShortMsg(const unsigned long message)
+{
+  midiOutShortMsg(Midi_Out_Handle, message);
+}
+
+void CALLBACK Midi_In_Proc(HMIDIIN handle, UINT msg,
+                           DWORD *instance, DWORD *p1, DWORD *p2)
+{
+  MIDIHDR *header_ptr;
+
+
+  switch (msg)
+  {
+    case MIM_LONGDATA:
+      // TBD: Handle SYSEX data input
+      header_ptr = (MIDIHDR *)p1;
+      rx_sysex++;
+
+      // Make the buffer free for next lot of SysEx data
+      header_ptr->dwBytesRecorded = 0;
+      midiInAddBuffer(Midi_In_Handle, &Midi_In_1, sizeof(Midi_In_1));
+      // TBD: Is this Ok?  (Should not be calling system fucntions in a callback)
+      break;
+
+    case MIM_DATA:
+      rx_msg++;
+      break;
+
+    case MIM_ERROR:
+    case MIM_CLOSE:
+      break;
+  }
+
+
+}
+
+UInt8 Midi_In_Open(int device)
+{
+  MMRESULT status;
+
+  status=midiInOpen(&Midi_In_Handle, device, (DWORD) Midi_In_Proc, 0, CALLBACK_FUNCTION);
+  if (status != MMSYSERR_NOERROR)
+  {
+    // TBD: Report open error details
+    return 1;
+  }
+
+  // TBD: Setup more than one MIDI in header/buffer
+  
+  // Setup MIDI in header
+  Midi_In_1.lpData = Midi_In_1_Buffer;
+  Midi_In_1.dwBufferLength = sizeof(Midi_In_1_Buffer);
+  Midi_In_1.dwFlags = 0;
+  status = midiInPrepareHeader(Midi_In_Handle, &Midi_In_1, sizeof(Midi_In_1));
+  if (status != MMSYSERR_NOERROR)
+  {
+    // TBD: Report header error details
+    return 1;
+  }
+
+  // Add a buffer to the MIDI in header
+  status=midiInAddBuffer(Midi_In_Handle, &Midi_In_1, sizeof(Midi_In_1));
+  if (status != MMSYSERR_NOERROR)
+  {
+    // TBD: Report buffer error details
+    return 1;
+  }
+
+  return 0;
+}
+
+void Midi_Get_Counts(int *msg_count_ptr, int *sysex_count_ptr)
+{
+  *msg_count_ptr=rx_msg;
+  *sysex_count_ptr=rx_sysex;
+}
