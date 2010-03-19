9662245f9a7f29a54ac91d5472eae1695a982290
ralley900 <ralley900@d8bc70b3-6028-4511-b56e-b43ad562b246>
2010-03-19 03:48:09 +0000

    Add source code directory

---
+++ src/quadedit.cpp
@@ -0,0 +1,34 @@
+
+
+
+#include <vcl.h>
+#include <stdio.h>
+#pragma hdrstop
+
+USERES("quadedit.res");
+USEFORM("main.cpp", MainForm);
+USEFORM("about.cpp", FormAbout);
+USEUNIT("midi.cpp");
+USEFORM("error.cpp", FormError);
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+   try
+   {
+      Application->Initialize();
+      Application->Title = "QuadraEdit";
+        Application->CreateForm(__classid(TMainForm), &MainForm);
+        Application->CreateForm(__classid(TFormAbout), &FormAbout);
+        Application->CreateForm(__classid(TFormError), &FormError);
+        Application->Run();
+   }
+   catch (Exception &exception)
+   {
+      Application->ShowException(&exception);
+   }
+   return 0;
+}
+
+
+
+
