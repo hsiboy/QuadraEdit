99f601eb170d60ce50d24e21ef9ca048477272b0
ralley900 <ralley900@d8bc70b3-6028-4511-b56e-b43ad562b246>
2010-03-19 03:52:11 +0000

    Add source code directory

---
+++ src/README.txt
@@ -0,0 +1,12 @@
+QuadEdit
+
+Capabilities required:
+
+   - Read a patch from the Quadraverb
+   - Write a patch to the Quadraverb
+   - Load a patch from a library of patches (on the PC disk)
+   - Save a patch to a library of patches (on the PC disk)
+   - Read a bank of patches from the Quadraverb
+   - Write a bank of patches to the Quadraverb
+   - Load a bank of patches from a library of patches (on the PC disk)
+   - Save a bank of patches to a library of patches (on the PC disk)
---
+++ src/about.h
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------
+#ifndef aboutH
+#define aboutH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+//---------------------------------------------------------------------------
+class TFormAbout : public TForm
+{
+__published:	// IDE-managed Components
+    TPanel *PanelAbout;
+    TLabel *LabelAbout1;
+    TLabel *LabelAbout2;
+    TButton *ButtonAboutOk;void __fastcall ButtonAboutOkClick(TObject *Sender);
+    void __fastcall PanelAboutExit(TObject *Sender);
+    void __fastcall FormShow(TObject *Sender);
+    void __fastcall FormHide(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+    __fastcall TFormAbout(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TFormAbout *FormAbout;
+//---------------------------------------------------------------------------
+#endif
